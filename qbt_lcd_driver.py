fcafcd55d93ad079ca7014852ab7099100eaea24
Scott Burlovich <2328416+blown5pt0@users.noreply.github.com>
2020-06-06 01:09:03 -0400

    Add dynamic suffixes for transfer rate and total transferred figures: bps, Kbps, Mbps - B, KB, MB, GB

--- qbt_lcd_driver.py
+++ qbt_lcd_driver.py
@@ -4,6 +4,27 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 import os
+
+def format_bytes(size):
+    power = 2**10
+    n = 0
+    power_labels = {0 : 'B', 1: 'KB', 2: 'MB', 3: 'GB', 4: 'TB'}
+    return_formats = {0: "{:.0f}", 1: "{:.0f}", 2: "{:.0f}", 3: "{:.2f}", 4: "{:.2f}"}
+    while size > power:
+        size /= power
+        n += 1
+    return return_formats[n].format(size) + power_labels[n]
+
+def format_bps(bps):
+    power = 2**10
+    n = 0
+    power_labels = {0 : 'bps', 1: 'Kbps', 2: 'Mbps'}
+    return_formats = {0: "{:.0f}", 1: "{:.0f}", 2: "{:.2f}"}
+    while bps > power:
+        bps /= power
+        n += 1
+    return return_formats[n].format(bps) + power_labels[n]
+
 qbt_client = qbittorrentapi.Client(host=os.getenv('WEBUI_HOST') + ':' + os.getenv('WEBUI_PORT'), username=os.getenv('WEBUI_USER'), password=os.getenv('WEBUI_PASSWORD'))
 
 try:
@@ -33,21 +54,29 @@ while True:
     # cycle between lines 1 & 2 then 3 & 4
     for i in range(10):
         transfer_info = qbt_client.transfer_info()
-        upload_speed = "{:.0f}".format((transfer_info.up_info_speed / 1000))
-        download_speed = "{:.0f}".format((transfer_info.dl_info_speed / 1000))
-        line1 = "UP:{up_speed}Kbps".format(up_speed=upload_speed)
-        line2 = "DN:{dl_speed}Kbps".format(dl_speed=download_speed)
+        #upload_speed = "{:.0f}".format((transfer_info.up_info_speed / 1000))
+        upload_speed = format_bps(transfer_info.up_info_speed)
+        download_speed = format_bps(transfer_info.dl_info_speed)
+        #download_speed = "{:.0f}".format((transfer_info.dl_info_speed / 1000))
+        line1 = "UP:{up_speed}".format(up_speed=upload_speed)
+        line2 = "DN:{dl_speed}".format(dl_speed=download_speed)
         # update LCD
-        subprocess.run([lcd_api_exe, '0', line1])
-        subprocess.run([lcd_api_exe, '1', line2])
+        #subprocess.run([lcd_api_exe, '0', line1])
+        #subprocess.run([lcd_api_exe, '1', line2])
+        print(line1)
+        print(line2)
         time.sleep(1)
 
     for i in range(10):
         transfer_info = qbt_client.transfer_info()
-        upload_total = "{:.0f}".format((transfer_info.up_info_data / 1000000))
-        download_total = "{:.0f}".format((transfer_info.dl_info_data / 1000000))
-        line3 = "UP:{up_total}MB".format(up_total=upload_total)
-        line4 = "DN:{dl_total}MB".format(dl_total=download_total)
-        subprocess.run([lcd_api_exe, '0', line3])
-        subprocess.run([lcd_api_exe, '1', line4])
+        #upload_total = "{:.0f}".format((transfer_info.up_info_data / 1000000))
+        upload_total = format_bytes(transfer_info.up_info_data)
+        download_total = format_bytes(transfer_info.dl_info_data)
+        #download_total = "{:.0f}".format((transfer_info.dl_info_data / 1000000))
+        line3 = "UP:{up_total}".format(up_total=upload_total)
+        line4 = "DN:{dl_total}".format(dl_total=download_total)
+        #subprocess.run([lcd_api_exe, '0', line3])
+        #subprocess.run([lcd_api_exe, '1', line4])
+        print(line3)
+        print(line4)
         time.sleep(1)
